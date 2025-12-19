0d93839c5f229d51858761fc3b99b27a588429d9
Katherine <maldonadolincangokatherinesil@gmail.com>
2025-12-19 12:24:44 -0500

    examen

--- resources/views/sesions/create.blade.php
+++ resources/views/sesions/create.blade.php
@@ -5,6 +5,15 @@
 @section('contenido')
 
     <h2 class="mb-4">Registrar Nueva Sesión</h2>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul class="mb-0">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
     <form action="{{ route('sesions.store') }}" method="POST">
         @csrf
